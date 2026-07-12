36ecfcaa632bc44d07c465eed44c488eb8dd93ca
Emir Şahin <emirsahin2116@outlook.com>
2026-07-13 00:06:19 +0300

    Initial Commit

---
+++ Animsat/AnimsatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimsatApp.swift
+//  Animsat
+//
+//  Created by Emir Şahin on 13.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimsatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
